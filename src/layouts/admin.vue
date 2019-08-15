c06e4154cf391e4f7f32812508c298bb06f00f8e
David Royer <droyer01@gmail.com>
2019-08-15 06:27:21 -0400

    update

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -1,25 +1,28 @@
 <template>
-  <section class="wrapper">
-    <app-notification :status="status" :type="type" :text="message"/>
-    <header>
-      <app-text tag-name="h1" class="medium">Administrator</app-text>
-	  <!-- <app-text tag-name="h2" class="medium">Test {{ user.email }}</app-text> -->
-    </header>
-    <section>
-      <ul class="list-container">
-        <li class="list-item">
-          <nuxt-link to="/admin/dashboard">Dashboard</nuxt-link>
-        </li>
-        <li class="list-item">
-          <nuxt-link to="/admin/change-password">Change Password</nuxt-link>
-        </li>
-        <li class="list-item">
-          <nuxt-link to="/admin/sign-out">Sign Out</nuxt-link>
-        </li>
-      </ul>
-    </section>
-    <nuxt-child/>
-  </section>
+	<section class="wrapper">
+		<app-notification :status="status" :type="type" :text="message" />
+		<header>
+			<app-text tag-name="h1" class="medium">Administrator</app-text>
+			<!-- <app-text tag-name="h2" class="medium">Test {{ user.email }}</app-text> -->
+		</header>
+		<section>
+			<ul class="list-container">
+				<li class="list-item">
+					<nuxt-link to="/">HOME</nuxt-link>
+				</li>
+				<li class="list-item">
+					<nuxt-link to="/admin/dashboard">Dashboard</nuxt-link>
+				</li>
+				<li class="list-item">
+					<nuxt-link to="/admin/change-password">Change Password</nuxt-link>
+				</li>
+				<li class="list-item">
+					<nuxt-link to="/admin/sign-out">Sign Out</nuxt-link>
+				</li>
+			</ul>
+		</section>
+		<nuxt-child />
+	</section>
 </template>
 
 <script>
@@ -28,21 +31,21 @@ import appNotification from '@/components/app-notification.vue';
 import appText from '@/components/app-text.vue';
 
 export default {
-	name: 'layouts-admin',
-	components: {
-		appNotification,
-		appText,
-	},
-	computed: {
-		...mapState({
-			message: state => state.notifications.message,
-			type: state => state.notifications.type,
-		}),
-		...mapGetters({
-			status: 'notifications/status',
-		}),
-	},
-	middleware: 'identity',
+  name: 'layouts-admin',
+  components: {
+    appNotification,
+    appText,
+  },
+  computed: {
+    ...mapState({
+      message: state => state.notifications.message,
+      type: state => state.notifications.type,
+    }),
+    ...mapGetters({
+      status: 'notifications/status',
+    }),
+  },
+  middleware: 'identity',
 };
 </script>
 
